80205a78b8ba5ccf778833f92ca618c0b5947416
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-03-24 19:00:13 +0300

    build minimum TCP-to-HTTP server

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
 	port := 8080
+
+	addr := fmt.Sprintf("0.0.0.0:%d", port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Printf("Server running on port %d...\n", port)
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	body := fmt.Sprintf("You sent:\n%s", buffer[:n])
+	statusLine := "HTTP/1.1 200 OK"
+	lenHeader := fmt.Sprintf("Content-Length: %d", len(body))
+	typeHeader := "Content-Type: text/plain"
+
+	res := fmt.Sprintf("%s\r\n%s\r\n%s\r\n\r\n%s", statusLine, lenHeader, typeHeader, body)
+	_, err = conn.Write([]byte(res))
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
