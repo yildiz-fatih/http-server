a469b75bc4fdcf2a47fc49bb37dec2bd42ad1122
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-03-25 13:26:07 +0300

    parse request line

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
+
+const SEPARATOR = "\r\n"
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpMethod    string
+	RequestTarget string
+	HttpVersion   string
+}
 
 func main() {
 	port := 8080
@@ -39,6 +52,17 @@ func handleConnection(conn net.Conn) {
 		return
 	}
 
+	request := string(buffer[:n])
+	reqLine, restOfMsg, err := parseRequestLine(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// test request line parsing
+	fmt.Printf("Parsed:\nMethod: %s, Target: %s, Version: %s\n", reqLine.HttpMethod, reqLine.RequestTarget, reqLine.HttpVersion)
+	fmt.Printf("Rest of the request:\n%s\n", restOfMsg)
+
 	body := fmt.Sprintf("You sent:\n%s", buffer[:n])
 	statusLine := "HTTP/1.1 200 OK"
 	lenHeader := fmt.Sprintf("Content-Length: %d", len(body))
@@ -50,4 +74,25 @@ func handleConnection(conn net.Conn) {
 		log.Println(err)
 		return
 	}
+}
+
+func parseRequestLine(request string) (*RequestLine, string, error) {
+	idx := strings.Index(request, SEPARATOR)
+	if idx == -1 {
+		return nil, request, fmt.Errorf("parsing error: no CRLF found")
+	}
+
+	line := request[:idx]
+	restOfMsg := request[idx+len(SEPARATOR):]
+
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return nil, restOfMsg, fmt.Errorf("parsing error: split by single space failed")
+	}
+
+	return &RequestLine{
+		HttpMethod:    parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   parts[2],
+	}, restOfMsg, nil
 }
